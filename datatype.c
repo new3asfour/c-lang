a92e417fc82d01f1863c5663b4dce27640f92c1d
new3asfour <46186757+new3asfour@users.noreply.github.com>
2019-01-15 22:03:39 +0200

    Add files via upload

---
+++ datatype.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+main()
+{
+    int a ;
+    float b ;
+    char c ;
+    double d ;
+    long e ;
+    printf(" a (int) = %d \n ",sizeof(a));
+    printf("b (float) = %d \n ",sizeof(b));
+    printf("c (char) = %d \n ",sizeof(c));
+    printf("d (double) = %d \n ",sizeof(d));
+    printf("e (long) =  %d \n ",sizeof(e));
+}
+
